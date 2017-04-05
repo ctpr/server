a10298e11b352eaa843b137bd0ebe6c94c32e94a
Chris Russ <chruss@gmx.de>
2017-04-06 08:16:20 +1000

    change from ROUTE to GET

--- start_server.py
+++ start_server.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
-from bottle import route, run
+# from bottle import route, run, template, static_file
+from bottle import get, post, request, run, static_file
 
-@route('/')
+
+@get('/') # @route('/')
 def hello():
     return "Hello SimScale! This is a tiny REST file server :)\n"
 
-@route('/geometry/<filename:re:.*>', method='GET')
-def send_ply(filename):
-    if "" == filename:
+@get('/geometry/<filename:re:.*>') # @route('/geometry/<filename:re:.*>', method='GET')
+def send_file(filename):
+    if "" != filename:
+        return static_file(filename, root='/data/geometry')
+    else:
         return { "success" : False, "error" : "Requested geometry called without a filename" }
-    return static_file(filename, root='/data/geometry')
 
 run(host='0.0.0.0', port=9000)
