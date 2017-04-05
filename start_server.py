155837268b5ae7ba47fa62ebaed8288910e3ce78
Chris Russ <chruss@gmx.de>
2017-04-06 07:14:01 +1000

    Add geometry files and serve them as static files

--- start_server.py
+++ start_server.py
@@ -4,6 +4,12 @@ from bottle import route, run
 
 @route('/')
 def hello():
-    return "Hello World!\n"
+    return "Hello SimScale! This is a tiny REST file server :)\n"
+
+@route('/geometry/<filename:re:.*\.ply>', method='GET')
+def send_ply(filename):
+    if "" == filename:
+        return { "success" : False, "error" : "Requested PLY geometry called without a filename" }
+    return static_file(filename, root='/data/geometry')
 
 run(host='0.0.0.0', port=9000)
