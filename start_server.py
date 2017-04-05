06348eb373cb0e625468b39163d6ca96c762312c
Chris Russ <chruss@gmx.de>
2017-04-06 08:31:20 +1000

    Cleanup and basic argument parsing

--- start_server.py
+++ start_server.py
@@ -1,18 +1,35 @@
 #!/usr/bin/env python
 
-# from bottle import route, run, template, static_file
+import sys, getopt
 from bottle import get, post, request, run, static_file
 
-
-@get('/') # @route('/')
+@get('/')
 def hello():
     return "Hello SimScale! This is a tiny REST file server :)\n"
 
-@get('/geometry/<filename:re:.*>') # @route('/geometry/<filename:re:.*>', method='GET')
+@get('/geometry/<filename:re:.*>')
 def send_file(filename):
     if "" != filename:
         return static_file(filename, root='/data/geometry')
     else:
         return { "success" : False, "error" : "Requested geometry called without a filename" }
 
-run(host='0.0.0.0', port=9000)
+def main(argv):
+    m_port = 9000
+    try:
+        opts, args = getopt.getopt(argv,"hp:",["port="])
+    except getopt.GetoptError:
+        print __file__+' -i <inputfile> -o <outputfile>'
+        sys.exit(1)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print __file__+' -p[--port] <port>'
+            sys.exit()
+        elif opt in ("-p", "--port"):
+            m_port = int(arg)
+
+    run(host='0.0.0.0', port=m_port)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
