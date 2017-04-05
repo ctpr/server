0d478c036c510eaaec32bc182521b83b4b6982c0
Chris Russ <chruss@gmx.de>
2017-04-06 07:24:51 +1000

    Do not limit to PLY file extension

--- start_server.py
+++ start_server.py
@@ -6,10 +6,10 @@ from bottle import route, run
 def hello():
     return "Hello SimScale! This is a tiny REST file server :)\n"
 
-@route('/geometry/<filename:re:.*\.ply>', method='GET')
+@route('/geometry/<filename:re:.*>', method='GET')
 def send_ply(filename):
     if "" == filename:
-        return { "success" : False, "error" : "Requested PLY geometry called without a filename" }
+        return { "success" : False, "error" : "Requested geometry called without a filename" }
     return static_file(filename, root='/data/geometry')
 
 run(host='0.0.0.0', port=9000)
