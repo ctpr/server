8eb03c681a255fce837ad33ed389c655d08dcc4b
Chris Russ <chruss@gmx.de>
2017-04-28 13:55:00 +1000

    Trying to make data directory configurable

--- start_server.py
+++ start_server.py
@@ -18,6 +18,7 @@ class CORS(object):
         return _cors
 
 app = Bottle()
+root = "/Users/chris/Documents/Work/SIMSCALE_2017/dev/server/data"
 
 # manually insert cross-origin resource sharing for static file response
 def static_file_crossdomain(*args, **kwargs):
@@ -28,13 +29,13 @@ def static_file_crossdomain(*args, **kwargs):
 # Generic server message
 @app.route('/', method=['OPTIONS', 'GET'])
 def hello():
-    return "You found the data server! Congratulations :)\n"
+    return "You found the data server! Congratulations :)\n" + root
 
 # List all available geometry paths
 @app.route('/geometries', method=['OPTIONS', 'GET'])
 def geometries_list():
     geometry_paths = []
-    geometry_files = os.listdir( '/data/geometry' )
+    geometry_files = os.listdir( root + '/geometry' )
     for geometry_file in geometry_files:
         if ".ply" == os.path.splitext( geometry_file )[1]:
             geometry_paths.append( geometry_file )
@@ -44,18 +45,19 @@ def geometries_list():
 @app.route('/geometries/<filename:re:.*>', method=['OPTIONS', 'GET'])
 def send_file(filename):
     if "" != filename:
-        return static_file_crossdomain(filename, root='/data/geometry')
+        return static_file_crossdomain(filename, root=root + '/geometry')
     else:
         return { "success" : False, "error" : "Requested geometry called without a filename" }
 
 # Help function
 def print_help():
-    print __file__+' -p --port <port> [9000]'
+    print __file__+' -p --port <port> [9000] -r --root <dir> [/data]'
 
 def main(argv):
     m_port = 9000
+    m_root = "/data"
     try:
-        opts, args = getopt.getopt(argv,"hp:",["port="])
+        opts, args = getopt.getopt(argv,"hpr:",["port=","root="])
     except getopt.GetoptError:
         print_help()
         sys.exit(1)
@@ -66,10 +68,15 @@ def main(argv):
             sys.exit()
         elif opt in ("-p", "--port"):
             m_port = int(arg)
+        elif opt in ("-r", "--root"):
+            # TODO: fix this
+            m_root = arg
 
+    root = m_root
     app.install(CORS())
 
     print "HTTP Bottle Server running on port: "+str(m_port)
+    print "Data expected in: "+root
     run(app, host='0.0.0.0', port=m_port)
 
 if __name__ == "__main__":
